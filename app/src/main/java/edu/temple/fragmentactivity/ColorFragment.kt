39973461e558fb4fcec96e3c85c65c717180f4d2
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-13 11:30:42 -0400

    added spinner functionality

--- app/src/main/java/edu/temple/fragmentactivity/ColorFragment.kt
+++ app/src/main/java/edu/temple/fragmentactivity/ColorFragment.kt
@@ -1,10 +1,13 @@
 package edu.temple.fragmentactivity
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 
@@ -14,13 +17,30 @@ class ColorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color, container, false).apply {
+        return inflater.inflate(R.layout.fragment_color, container, false)
+    }
+
+    //                          v the layout that was inflated from above
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.apply {
             findViewById<Spinner>(R.id.spinner).apply {
-                //                        v cannot reference "this" because fragments don't have context
                 adapter = ArrayAdapter(context, android.R.layout.simple_spinner_item,
                     arrayOf("Blue", "Red", "White", "Black", "Magenta"))
 
-                // onItemSelectedListener =
+                onItemSelectedListener = object: OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, Id: Long) {
+                        parent?.run {
+                            view?.setBackgroundColor(Color.parseColor(getItemAtPosition(position).toString()))
+                        }
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
+
+                }
             }
         }
     }
